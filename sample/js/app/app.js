a89fe2b13441ac4f281fda38ca9e64abce18dab7
Joseph Layman <laymanje@gmail.com>
2016-06-23 13:54:51 -0400

    nav for elements

--- sample/js/app/app.js
+++ sample/js/app/app.js
@@ -2,4 +2,53 @@
 'use strict';
 
 var App = angular.module('App',
-    ['ui.bootstrap']);
+    ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
+
+App.config(['$routeProvider', '$locationProvider', '$httpProvider',
+    function ($routeProvider, $locationProvider, $httpProvider) {
+
+        $routeProvider
+
+            .when('/', {
+                templateUrl: '../../templates/colors.html'
+            })
+
+            .when('/buttons', {
+                templateUrl: '../../templates/buttons.html'
+            })
+
+            .when('/inputs', {
+                templateUrl: '../../templates/inputs.html'
+            })
+
+            .when('/tabs', {
+                templateUrl: '../../templates/tabs.html'
+            })
+
+            .when('/nestedtabs', {
+                templateUrl: '../../templates/nested-tabs.html'
+            })
+
+            .when('/list-single', {
+                templateUrl: '../../templates/list-single-col.html'
+            })
+
+            .when('/list-multi', {
+                templateUrl: '../../templates/list-multi-col.html'
+            })
+
+            .when('/table', {
+                templateUrl: '../../templates/table.html'
+            })
+
+        ;
+
+    }
+]);
+
+App.controller('MainCtrl',
+    ['$scope',
+    function ($scope) {
+
+    }
+]);
